f792f91f35e8db2a574dd71f7be408ae0a3646e6
Raymond Tse <tser@rpi.edu>
2014-04-13 03:18:55 -0400

    Added PlayerDrawer class

---
+++ MJ-android/src/com/real/mahjong/PlayerDrawer.java
@@ -0,0 +1,22 @@
+package com.real.mahjong;
+
+//import com.real.mahjong.Player;
+
+public class PlayerDrawer {
+	
+	public PlayerDrawer() {
+		
+	}
+	/*
+	public void Draw(Player p) {
+		
+		// Check if player is the main user
+		//if (p) is mainplayer
+			drawDiscard(p);
+			drawAside(p);
+			drawHand(p);
+		
+	}
+
+	*/
+}
