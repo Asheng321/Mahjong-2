08c1f07f20625a39e4f1ba0ed13289938b8b9ca4
Raymond Tse <tser@rpi.edu>
2014-04-12 22:45:42 -0400

    Added main Mahjong class

---
+++ MJ-android/src/com/me/mygdxgame/Mahjong.java
@@ -0,0 +1,36 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Mahjong implements ApplicationListener {
+	
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+	
+	@Override
+	public void render() {
+		
+	}
+	
+	@Override
+	public void pause() {
+		
+	}
+	
+	@Override
+	public void resume() {
+		
+	}
+	
+	@Override
+	public void dispose() {
+		
+	}
+}
